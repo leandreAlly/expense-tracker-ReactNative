7816f6dd87041de5be961856f9012b24bd2f3ee3
leandre <tuyambazeleandre@gmail.com>
2024-02-14 21:13:21 +0200

    Setup expenses track files

---
+++ components/ExpenseOutput/ExpensesOutput.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View } from 'react-native';
+import ExpensesList from './ExpensesList';
+import ExpensesSummary from './ExpensesSummary';
+
+function ExpensesOutput({ expenses }) {
+  return (
+    <View>
+      <ExpensesSummary />
+      <ExpensesList />
+    </View>
+  );
+}
+
+export default ExpensesOutput;
