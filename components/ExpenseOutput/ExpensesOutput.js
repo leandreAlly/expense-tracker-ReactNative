2ecbbc739ce578e852565735f4eca6f473a0cb49
leandre <tuyambazeleandre@gmail.com>
2024-02-15 19:40:52 +0200

    Outputing the expenses on screen

--- components/ExpenseOutput/ExpensesOutput.js
+++ components/ExpenseOutput/ExpensesOutput.js
@@ -3,11 +3,38 @@ import { View } from 'react-native';
 import ExpensesList from './ExpensesList';
 import ExpensesSummary from './ExpensesSummary';
 
-function ExpensesOutput({ expenses }) {
+const DUMMY_EXPENSES = [
+  {
+    id: 'e1',
+    title: 'New Shoes',
+    amount: 99.99,
+    date: new Date('2021-07-14'),
+  },
+  {
+    id: 'e2',
+    title: 'New Shirt',
+    amount: 49.99,
+    date: new Date('2021-07-15'),
+  },
+  {
+    id: 'e3',
+    title: 'New Pants',
+    amount: 89.99,
+    date: new Date('2021-07-16'),
+  },
+  {
+    id: 'e4',
+    title: 'New Hat',
+    amount: 19.99,
+    date: new Date('2022-07-17'),
+  },
+];
+
+function ExpensesOutput({ expenses, expensesPeriod }) {
   return (
     <View>
-      <ExpensesSummary />
-      <ExpensesList />
+      <ExpensesSummary expenses={DUMMY_EXPENSES} periodName={expensesPeriod} />
+      <ExpensesList expenses={DUMMY_EXPENSES} />
     </View>
   );
 }
